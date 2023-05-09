3d75401038a7bd0feaffc410dc76b23e089fbbb4
kawasakiyuto <kawasakiyuto@localhost>
2023-05-09 14:31:17 +0900

    初期構築

---
+++ e-learning/src/test/java/com/hap/api/ELearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hap.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
